07e0b2c4a5b93ff911088f5593b11f016549f5a4
Priyanshu Vaibhav <priyanshuvabhv17@gmail.com>
2026-01-20 13:12:56 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function clockStarter(){
+    const now = new Date();
+    let hour = now.getHours().toString().padStart(2,0);
+    let mederian = hour>=12 ?  "PM" : "AM";
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const seconds = now.getSeconds().toString().padStart(2,0);
+    document.getElementById("clock").textContent = `${hour}:${minutes}:${seconds}${mederian}`
+
+}
+
+clockStarter();
+setInterval(clockStarter,1000);
