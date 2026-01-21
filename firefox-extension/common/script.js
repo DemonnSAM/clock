fbd3e7f222cb1dcfd2c79730accd168660bbbec8
Priyanshu Vaibhav <priyanshuvabhv17@gmail.com>
2026-01-21 10:21:59 +0000

    Added date and liquid glass effect

--- firefox-extension/common/script.js
+++ firefox-extension/common/script.js
@@ -6,6 +6,16 @@ function clockStarter(){
     const seconds = now.getSeconds().toString().padStart(2,0);
     document.getElementById("clock").textContent = `${hour}:${minutes}:${seconds}${mederian}`
 }
-
 clockStarter();
 setInterval(clockStarter,1000);
+
+function updateDate(){
+    const now = new Date();
+    const day = now.toLocaleDateString('en-US',{weekday:'long'});
+    const month = now.toLocaleDateString('en-US',{month: 'short'});
+    const date = now.getDate();
+    const year = now.getFullYear();
+    document.getElementById("date").textContent = `${month} ${date},${year} | ${day}`;
+}
+setInterval(updateDate, 60000);
+updateDate();
